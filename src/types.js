f2059e8dbdf91afb397cdc576d1ca9bfa8d76110
Jostein Berre Eliassen <josteinpost@gmail.com>
2016-04-30 17:22:20 -0400

    basic atom implementation

--- src/types.js
+++ src/types.js
@@ -92,7 +92,193 @@ const types = module.exports = {
       fail:     (msg) => queue.put(['danger',  msg]),
       error:    (msg) => queue.put(['danger',  msg])
     };
+  },
+
+  persistent: function(ctor, name, defaultValue) {
+    let item = localStorage.getItem(name) || defaultValue;
+    let variable = ctor(item);
+    tracker.autorun(() => localStorage.setItem(name, variable.get()));
+    return variable;
   }
+
 }
 
 
+
+function eachKV(obj, fn) {
+  Object.keys(obj).forEach((k) => fn(k, obj[k]));
+}
+
+class Atom {
+  constructor(val) {
+    this._dep = new tracker.Dependency();
+    if (this.subConstructor) this.subConstructor();
+    if (val !== undefined) this.set(val);
+  }
+
+  // Return the sub-atom at the given location, or throw an error if it
+  // does not exist.
+  sel(...path) {
+    let fragments = [].concat.apply(
+      path.map((p) => typeof(p) == "string" ? p.split('/') : p))
+      .filter((p) => p !== undefined);
+    return fragments.length == 0
+      ? this
+      : this._sel(fragments);
+  }
+
+  _strictSel(path) {
+    let subAtom = this.sel(...path);
+    if (!subAtom) {
+      console.log(path);
+      throw new Error('Nonexistent scope.');
+    }
+    return subAtom;
+  }
+
+  // Returns the value of this atom or an optionally specified sub-atom
+  get(...path) {
+    let atom = this._strictSel(path);
+    atom._dep.depend();
+    return atom._get();
+  }
+
+  // Sets the value of this atom or an optionally specified sub-atom
+  set(...pathAndVal) {
+    let
+      val    = pathAndVal.pop(),
+      path   = pathAndVal;
+    return this.sel(...path)._set(val);
+  }
+
+  _changed() {
+    this._dep.changed();
+    if (this._parent) this._parent._changed();
+  }
+
+  modify(fn) { this.set(fn(this.get())); }
+}
+
+class ValueAtom extends Atom {
+  _validate(val) {
+    let valid = !val
+      || typeof(val) == 'boolean'
+      || typeof(val) == 'string'
+      || typeof(val) == 'number';
+    if (valid) return;
+    console.log(val);
+    throw new Error('Value atom expected string, number or falsey type.');
+  }
+
+  _sel()    { return undefined; }
+  _get(val) { return this._val }
+
+  _set(val) {
+    if (this._val === val) return false;
+    this._validate(val);
+    this._val = val;
+    this._changed();
+    return true;
+  }
+
+  inc()   { this._val++; this._changed(); }
+  dec()   { this._val--; this._changed(); }
+  flip()  { this._val = !this._val; this._changed(); }
+}
+
+class ListOrObjectAtom extends Atom {
+  subConstructor() {
+    this._subAtoms = new this._getBaseType()();
+  }
+
+  _validate(val) {
+    if (val.constructor !== this._getBaseType()) {
+      console.log(val);
+      throw new Error("Expected an " + this._getBaseType().name);
+    }
+  }
+
+  _sel(...scope) {
+    let
+      key = scope.shift(),
+      sub = this._subAtoms[key];
+    if (!sub) return undefined;
+    return sub.sel(...scope);
+  }
+
+  _get() {
+    let r = new this._getBaseType()();
+    eachKV(this._subAtoms, (k, v) => r[k] = v._get());
+    return r;
+  }
+
+  _set(val) {
+    this._validate(val);
+    let changed = false;
+    eachKV(val, (k, v) => {
+      if (this._subAtoms[k] === undefined) {
+        this._subAtoms[k] = atom(v, this);
+        this._subAtoms[k]._parent = this;
+        changed = true;
+      }
+      else if (this._subAtoms[k]._set(v)) changed = true;
+    });
+    return changed;
+  }
+}
+
+class ListAtom extends ListOrObjectAtom {
+  _getBaseType() { return Array; }
+
+  push(val) {
+    this._subAtoms.push(atom(val));
+    this._dep.changed();
+  }
+
+  pop(val) {
+    if (this._subAtoms.length == 0) return undefined;
+    this._dep.changed();
+    return this._subAtoms.pop().get();
+  }
+
+  shift(val) {
+    if (this._subAtoms.length == 0) return undefined;
+    this._dep.changed();
+    return this._subAtoms.shift().get();
+  }
+
+  unshift(val) {
+    this._subAtoms.unshift(atom(val));
+    this._dep.changed();
+  }
+
+  // TODO: drop, filter, insert
+}
+
+class ObjectAtom extends ListOrObjectAtom {
+  _getBaseType() { return Object; }
+}
+
+// function atomFromKey(k) {
+//   switch (k.constructor) {
+//     case String:  return atom({});
+//     case Number:  return atom([]);
+//     default:
+//       console.log(k);
+//       throw "Unsupported atom key type.";
+//   }
+// }
+
+function atom(v) {
+  switch (v.constructor) {
+    case Array:   return new ListAtom(v);
+    case Object:  return new ObjectAtom(v);
+    case String:
+    case Number:  return new ValueAtom(v);
+    default:
+      console.log(v);
+      throw new Error("Unsupported Atom type.");
+  }
+}
+
+types.atom = atom;
